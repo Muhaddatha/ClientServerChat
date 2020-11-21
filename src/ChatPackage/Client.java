33c996184ed06d67d95bc64e14cb4ab6ae1506ce
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2020-11-21 18:43:48 -0500

    initial commit

---
+++ src/ChatPackage/Client.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChatPackage;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 3pear
+ */
+public class Client extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+    
+    public static void main(String[] args){
+        launch(args);
+    }
+}
