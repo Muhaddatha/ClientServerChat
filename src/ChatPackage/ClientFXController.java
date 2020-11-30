193ae674be907859ea807baf336b8120b4f05a85
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2020-11-30 12:09:15 -0500

    fixed fxml issue

--- src/ChatPackage/ClientFXController.java
+++ src/ChatPackage/ClientFXController.java
@@ -5,15 +5,15 @@
  */
 package ChatPackage;
 
+import com.sun.javafx.logging.PlatformLogger.Level;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.System.Logger;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -24,35 +24,69 @@ import javafx.scene.control.TextField;
 /**
  * FXML Controller class
  *
- * 
+ * @author 3pear
  */
-public class ClientFXMLController implements Initializable, Runnable {
+public class ClientFXController implements Initializable, Runnable {
 
+//    private TextArea textArea;
+//    @FXML
+    @FXML
+    private TextField textField;
     @FXML
     private Button sendButton;
-    @FXML
-    private TextField inputTextbox;
-//    @FXML
-//    private TextArea window; 
+
+    /**
+     * Initializes the controller class.
+     */
+    
     static boolean debugging = true;
-  
+    
+    //variables needed to connect to the server
     final static String SERVER_IP_ADDR = "127.0.0.1";
     final static int SERVER_PORT_NUM = 9080;
     
+//    BufferedReader input = null;
+//    BufferedReader keyboard = null;
+//    PrintWriter out = null;
+//    
     
-        
-    //client's socket connecting to server
     static String message = "";
+//    private TextField textbox;
+    @FXML
+    private TextArea chatLog;
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleButton(ActionEvent event) {
         
+        if("".equals(textField.getText())){
+            
+            if(debugging){
+                System.out.println("Error: empty message cannot be sent");
+            }
+            
+            textField.setPromptText("Error: Please enter a message to send.");
+            
+        }
+        else{
+            message = textField.getText();
+            textField.clear();
+            chatLog.appendText("YOU: " + message + "\n");
+            
+            if(debugging){
+                System.out.println("Message to send= " + message);
+            }
+        }
     }
     
+    
     public static void main(String[] args) throws IOException{
-        
-       Socket socket = null;
+        Socket socket = null;
         if(debugging){
             System.out.println("Inside initialize method in the controller.");
         }
@@ -61,7 +95,7 @@ public class ClientFXMLController implements Initializable, Runnable {
             socket = new Socket(SERVER_IP_ADDR, SERVER_PORT_NUM);
             if(debugging){System.out.println("Sucessfully connected to server");}
         } catch (IOException ex) {
-            Logger.getLogger(ClientFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(ClientFXMLController.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         BufferedReader input = new BufferedReader (new InputStreamReader(socket.getInputStream()));
@@ -69,39 +103,10 @@ public class ClientFXMLController implements Initializable, Runnable {
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         
     }
-        
-    @FXML
-    private void handleButton(ActionEvent event) {
-        //1. Check to see if there is a message fto send
-        if("".equals(inputTextbox.getText())){
-            //The input textfield is empty, so display error
-            if(debugging){
-                System.out.println("Error: Empty message cannot be sent");
-            }
-            inputTextbox.setPromptText("Error: Please enter a message to send.");
-            //make the error message text red.
-        }
-        else{
-            //send message to server
-            message = inputTextbox.getText();
-
-            inputTextbox.clear();
-            if(debugging){
-                System.out.println("Sending message (" + message + ") to server");
-            }
-            
-        
-        }
-    }
 
     @Override
     public void run() {
-        //
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
-    
-    
-    
     
 }
